dc0466962e0fa603161c665ccaa318aed2e33753
Tchomkem <Ryan.Junior.Tchomkem@etu.univ-st-etienne.fr>
2022-01-03 08:41:54 +0100

    Create HomeController.php

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+	/**
+     * @Route("/actu", name="actu")
+     */
+    public function actu(): Response
+    {
+        return $this->render('home/actu.html.twig', [
+            'controller_name' => 'salut',
+        ]);
+    }
+	/**
+     * @Route("/loisires", name="loisires")
+     */
+    public function loisires(): Response
+    {
+        return $this->render('home/loisires.html.twig', [
+            'controller_name' => 'salut',
+        ]);
+    }
+}
